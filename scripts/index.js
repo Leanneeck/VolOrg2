4406a2c77961274f9db27514e4283593195762b8
Leanne Eckelberg <leanne.eckelberg@gmail.com>
2019-07-29 22:23:13 -0600

    Added Dockerfile. Begain Dockerizing.

--- scripts/index.js
+++ scripts/index.js
@@ -48,9 +48,43 @@ const setupGuides = (data) => {
 
         guideList.innerHTML = html;
     } else {
-        guideList.innerHTML = '<h5 class="center-align">Where Volunteers and Organizations Connect</h> ' +
-            '<br> <img src="img/girldog.jpg" alt="Girl with dog" width="500" height="600" class="center">' +
-            '<h5 class="center-align">Login to view profiles</h>'
+        guideList.innerHTML = '<div class="slider">' +
+            '<ul class="slides">' +
+            '<li>' +
+            '<img src="img/girldog.jpg"> <!-- random image -->' +
+            '<div class="caption center-align">' +
+            '<h3>This is our big Tagline!</h3>' +
+            '<h5 class="light grey-text text-lighten-3">Here\'s our small slogan.</h5>' +
+            '</div>' +
+            '</li>' +
+            '<li>' +
+            '<img src="https://lorempixel.com/580/250/nature/2"> <!-- random image -->' +
+            '<div class="caption left-align">' +
+            '<h3>Left Aligned Caption</h3>' +
+            '<h5 class="light grey-text text-lighten-3">Here\'s our small slogan.</h5>' +
+            '</div>' +
+            '</li>' +
+            '<li>' +
+            '<img src="https://lorempixel.com/580/250/nature/3"> <!-- random image -->' +
+            '<div class="caption right-align">' +
+            '<h3>Right Aligned Caption</h3>' +
+            '<h5 class="light grey-text text-lighten-3">Here\'s our small slogan.</h5>' +
+            '</div>' +
+            '</li>' +
+            '<li>' +
+            '<img src="https://lorempixel.com/580/250/nature/4"> <!-- random image -->' +
+            '<div class="caption center-align">' +
+            '<h3>This is our big Tagline!</h3>' +
+            '<h5 class="light grey-text text-lighten-3">Here\'s our small slogan.</h5>' +
+            '</div>' +
+            '</li>' +
+            '</ul>' +
+            '</div>'
+            // '<img class="responsive-img" src="img/girldog.jpg" alt="Girl with dog">' +
+            // '<h5 class="center-align">Where Volunteers and Organizations Connect</h>' +
+            // '<br>'+
+            // '<img src="img/girldog.jpg" alt="Girl with dog" width="500" height="600" class="center">' +
+            // '<h5 class="center-align">Login to view profiles</h>'
     }
 }
 
@@ -62,5 +96,9 @@ document.addEventListener('DOMContentLoaded', function() {
   
     var items = document.querySelectorAll('.collapsible');
     M.Collapsible.init(items);
+
+    var elems = document.querySelectorAll('.slider');
+
+    var instances = M.Slider.init(elems, options);
   
   });
